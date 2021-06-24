406f0789f3704fd740ca20e38baec33a3f47c009
haderman <hadermania@gmail.com>
2021-06-24 15:38:05 +0300

    test fauna db

--- components/text.tsx
+++ components/text.tsx
@@ -3,6 +3,7 @@ import axios from 'axios';
 
 export default function Text() {
   const [message, setMessage] = useState('');
+  const [loginMsg, setLoginMsg] = useState('idle');
   useEffect(() => {
     updateMessage();
   }, []);
@@ -20,12 +21,33 @@ export default function Text() {
       });
   }
 
+  function login() {
+    setLoginMsg('login...');
+    axios
+      .post('/api/login')
+      .then(res => {
+        setLoginMsg(res.data);
+      })
+      .catch(err => {
+        setLoginMsg('error ' + err);
+        console.error('error ', err);
+      });
+  }
+
   return (
-    <div style={{ padding: 10, backgroundColor: 'blanchedalmond', width: '100%', marginBottom: 20 }}>
-      <span style={{ marginRight: 20 }}>TEXT: {message}</span>
-      <button type="button" onClick={updateMessage} style={{ padding: 20 }}>
-        Update
-      </button>
+    <div style={{ display: 'flex', flexDirection: 'column', gap: '20px', justifyContent: 'flex-start', alignItems: 'flex-start', width: '100%' }}>
+      <div style={{ padding: 10, backgroundColor: 'blanchedalmond', width: '100%', marginBottom: 20 }}>
+        <button type="button" onClick={updateMessage} style={{ padding: 20, width: 100, marginRight: 10 }}>
+          Update
+        </button>
+        <span style={{ marginRight: 20 }}>TEXT: {message}</span>
+      </div>
+      <div style={{ padding: 10, backgroundColor: 'blanchedalmond', width: '100%', marginBottom: 20 }}>
+        <button type="button" onClick={login} style={{ padding: 20, width: 100, marginRight: 10  }}>
+          Login
+        </button>
+        <span style={{ marginRight: 20 }}>LOGIN TEXT: {loginMsg}</span>
+      </div>
     </div>
   )
 }
---
+++ pages/api/login.ts
@@ -0,0 +1,44 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+import faunadb, { query as q } from 'faunadb';
+import { uuid } from 'uuidv4';
+
+const client = new faunadb.Client({
+  secret: process.env.FAUNA_ACCESS_KEY_ADMIN as string,
+});
+
+export default (req: NextApiRequest, res: NextApiResponse<string>) => {
+  if (req.method !== 'POST') {
+    res.status(404).send('this route does not exist');
+    return;
+  }
+
+  createAuthToken()
+    .then(({ result, token }) => {
+      if (!result.ref) {
+        return res.status(404).send('token ref is missing');
+      }
+      res.status(201).send(token);
+    })
+    .catch(error => {
+      res.status(error.requestResult.statusCode).send(error);
+    });
+}
+
+function createAuthToken(): Promise<{ result: any, token: string }> {
+  return new Promise((resolve, reject) => {
+    const token = uuid();
+    client.query(
+      q.Create(q.Collection('Auth'), {
+        data: {
+          token,
+        }
+      })
+    )
+      .then((result) => {
+        resolve({ result, token });
+      })
+      .catch(err => {
+        reject(err);
+      });
+  });
+}
